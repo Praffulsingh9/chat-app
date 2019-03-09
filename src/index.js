f9147db8300cbbdba6e152ed76b9b7c931a4a6d3
Praffulsingh9 <prafful.singh017@gmail.com>
2019-03-09 13:04:45 +0530

    Redux store was setup to manage global state

--- src/index.js
+++ src/index.js
@@ -1,20 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import App from "./components/App";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import "semantic-ui-css/semantic.min.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from "react-router-dom";
+import firebase from "./firebase";
+import rootReducer from "./reducers/index";
+import { setUser } from "./actions";
+const store = createStore(rootReducer, composeWithDevTools());
 
-const Root = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </Switch>
-  </Router>
+class Root extends Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.setUser(user);
+        this.props.history.push("/");
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+const RootWithRouter = withRouter(
+  connect(
+    null,
+    { setUser }
+  )(Root)
 );
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <RootWithRouter />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
