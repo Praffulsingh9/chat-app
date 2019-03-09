136d0168ad39d490257f34869d88980877aecf01
Praffulsingh9 <prafful.singh017@gmail.com>
2019-03-09 23:25:37 +0530

    Color Panel and Messages Component scaffolded

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
-
+import { Divider, Menu, Sidebar, Button } from "semantic-ui-react";
 class ColorPanel extends Component {
   render() {
-    return <div>ColorPanel</div>;
+    return (
+      <Sidebar
+        as={Menu}
+        vertical
+        icon="labeled"
+        inverted
+        visible
+        width="very thin"
+      >
+        <Divider />
+        <Button icon="add" size="small" color="blue" />
+      </Sidebar>
+    );
   }
 }
 
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
-
+import { Segment, Comment } from "semantic-ui-react";
+import MessagesHeader from "./MessagesHeader";
+import MessageForm from "./MessageForm";
 class Messages extends Component {
   render() {
-    return <div>Messages</div>;
+    return (
+      <React.Fragment>
+        <MessagesHeader />
+
+        <Segment>
+          <Comment.Group className="messages">HELLO</Comment.Group>
+        </Segment>
+
+        <MessageForm />
+      </React.Fragment>
+    );
   }
 }
 
