8b06273e740956883dc45d0b739d7240820c02fc
Praffulsingh9 <prafful.singh017@gmail.com>
2019-03-09 15:26:59 +0530

    Basic structure of chatConsole was setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
 import "./App.css";
-
-class App extends Component {
-  render() {
-    return <div>App</div>;
-  }
-}
+import GridColumn, { Grid } from "semantic-ui-react";
+import ColorPanel from "./ColorPanel/ColorPanel";
+import SidePanel from "./SidePanel/SidePanel";
+import Messages from "./Messages/Messages";
+import MetaPanel from "./MetaPanel/MetaPanel";
+const App = () => (
+  <Grid columns="equal" className="app" style={{ background: "#eee" }}>
+    <ColorPanel />
+    <SidePanel />
+    <Grid.Column style={{ marginLeft: 320 }}>
+      <Messages />
+    </Grid.Column>
+    <Grid.Column width={4}>
+      <MetaPanel />
+    </Grid.Column>
+  </Grid>
+);
 
 export default App;
