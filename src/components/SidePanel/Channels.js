e04e76fb17ab77319021339059514722af264819
Praffulsingh9 <prafful.singh017@gmail.com>
2019-03-09 20:50:12 +0530

    Displaying channels

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -12,6 +12,26 @@ class Channels extends Component {
     user: this.props.currentUser
   };
 
+  componentDidMount() {
+    this.addListeners();
+  }
+
+  addListeners = () => {
+    let loadedChannels = [];
+    this.state.channelsRef.on("child_added", snap => {
+      loadedChannels.push(snap.val());
+      this.setState({ channels: loadedChannels });
+    });
+  };
+
+  displayChannels = channels =>(
+      channels.length > 0 && channels.map(channel => (
+          <Menu.Item key={channel.id} name={channel.name} style={{opacity:0.7}} onClick={()=>console.log(channel)}>
+           # {channel.name}
+          </Menu.Item>
+      ))
+  )
+
   addChannel = () => {
     const { channelsRef, channelName, channelDetails, user } = this.state;
 
@@ -64,6 +84,7 @@ class Channels extends Component {
             </span>{" "}
             ({channels.length}) <Icon name="add" onClick={this.openModal} />
           </Menu.Item>
+          {this.displayChannels(channels)}
         </Menu.Menu>
 
         <Modal basic open={modal} onClose={this.closeModal}>
