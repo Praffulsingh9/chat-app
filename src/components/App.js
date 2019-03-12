ae99cb2fd9ef1fa07ef725f6ea9f324b1089c925
Praffulsingh9 <prafful.singh017@gmail.com>
2019-03-12 23:21:23 +0530

    Display Top Posters of channel in MetaPanel

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import ColorPanel from "./ColorPanel/ColorPanel";
 import SidePanel from "./SidePanel/SidePanel";
 import Messages from "./Messages/Messages";
 import MetaPanel from "./MetaPanel/MetaPanel";
-const App = ({ currentUser, currentChannel, isPrivateChannel }) => (
+const App = ({ currentUser, currentChannel, isPrivateChannel, userPosts }) => (
   <Grid columns="equal" className="app" style={{ background: "#eee" }}>
     <ColorPanel />
     <SidePanel key={currentUser && currentUser.id} currentUser={currentUser} />
@@ -23,6 +23,7 @@ const App = ({ currentUser, currentChannel, isPrivateChannel }) => (
         key={currentChannel && currentChannel.id}
         currentChannel={currentChannel}
         isPrivateChannel={isPrivateChannel}
+        userPosts={userPosts}
       />
     </Grid.Column>
   </Grid>
@@ -31,7 +32,8 @@ const App = ({ currentUser, currentChannel, isPrivateChannel }) => (
 const mapStateToProps = state => ({
   currentUser: state.user.currentUser,
   currentChannel: state.channel.currentChannel,
-  isPrivateChannel: state.channel.isPrivateChannel
+  isPrivateChannel: state.channel.isPrivateChannel,
+  userPosts: state.channel.userPosts
 });
 
 export default connect(mapStateToProps)(App);
--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { Segment, Accordion, Header, Icon, Image } from "semantic-ui-react";
+import {
+  Segment,
+  Accordion,
+  Header,
+  Icon,
+  Image,
+  List
+} from "semantic-ui-react";
 class MetaPanel extends Component {
   state = {
     activeIndex: 0,
@@ -13,8 +20,26 @@ class MetaPanel extends Component {
     const newIndex = activeIndex === index ? -1 : index;
     this.setState({ activeIndex: newIndex });
   };
+
+  formatCount = num => (num > 1 || num === 0 ? `${num} posts` : `${num} post`);
+
+  displayTopUsers = posts =>
+    Object.entries(posts)
+      .sort((a, b) => b[1] - a[1])
+      .map(([key, val], i) => (
+        <List.Item key={i}>
+          <Image avatar src={val.avatar} />
+          <List.Content>
+            <List.Header as="a">{key}</List.Header>
+            <List.Description>{this.formatCount(val.count)}</List.Description>
+          </List.Content>
+        </List.Item>
+      ))
+      .slice(0, 5);
+
   render() {
     const { activeIndex, privateChannel, channel } = this.state;
+    const { userPosts } = this.props;
     if (privateChannel) return null;
     return (
       <Segment loading={!channel}>
@@ -44,7 +69,7 @@ class MetaPanel extends Component {
             Top Posters
           </Accordion.Title>
           <Accordion.Content active={activeIndex === 1}>
-            posters
+            <List>{userPosts && this.displayTopUsers(userPosts)}</List>
           </Accordion.Content>
           <Accordion.Title
             active={activeIndex === 2}
